a41d325e68dcb73a1c9413390d793c2011c011b4
stanislav <andropa07stas@gmail.com>
2023-08-16 23:24:24 +0300

    add pick image from gallery

--- app/src/main/java/com/example/ive/ui/discover/add_photo/AddPhotoFragment.kt
+++ app/src/main/java/com/example/ive/ui/discover/add_photo/AddPhotoFragment.kt
@@ -1,8 +1,29 @@
 package com.example.ive.ui.discover.add_photo
 
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.ive.databinding.FragmentAddPhotoBinding
 import com.example.ive.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AddPhotoFragment : BaseFragment<FragmentAddPhotoBinding>() {
+
+    private val galleryImage = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) {
+        binding.ivImage.setImageURI(it)
+    }
+
+    override fun initListeners() {
+        binding.btAddPhoto.setOnClickListener {
+            galleryImage.launch(IMAGE_MIME_TYPE)
+        }
+    }
+
     override fun getDataBinding() = FragmentAddPhotoBinding.inflate(layoutInflater)
+
+    companion object {
+        const val IMAGE_MIME_TYPE = "image/*"
+    }
 }
